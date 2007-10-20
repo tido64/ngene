abe9114c5863aeec364fb4937973448671f5031a
tn0502 <tn0502@localhost>
2007-10-20 03:36:07 +0000

    Removing unnecessary files.

--- modules/.deprecated/selector_fitness_proportionate/selector_fitness_proportionate.h
+++
@@ -1,9 +0,0 @@
-#include "../../Ngene/Interfaces/Selector.h"
-
-namespace FitnessProportionate
-{
-	std::vector<double> roulette;
-	double random() { return (double)rand() / ((double)RAND_MAX + 1.0); }
-}
-
-const char *name() { return "Fitness Proportionate"; }
