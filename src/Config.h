c95e8ca90ea1ab658d1b4eeaf80d8dc730445ff2
tn0502 <tn0502@localhost>
2007-11-18 13:37:34 +0000

    Config struct.

---
+++ src/Config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_STRUCT
+#define CONFIG_STRUCT
+
+#include <vector>
+
+struct Config
+{
+	bool elitism;
+	double mating_rate, mutation_rate;
+	int adult_pool_capacity,
+		doomsday,
+		lifespan,
+		max_prodigies,
+		offspring_rate;
+	std::vector<std::string> module_path, parameters;
+};
+
+#endif
