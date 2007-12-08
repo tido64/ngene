9dae73a0a77ed37ba0b43347aee2461d76e7f925
tn0502 <tn0502@localhost>
2007-12-08 03:44:35 +0000

    Initial import

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ngene_Configurator
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Configurator());
+		}
+	}
+}
