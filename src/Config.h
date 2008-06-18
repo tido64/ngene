c35b402f5873ccfd87e88060cae45bd312167928
Tommy Nguyen <tn0502@gmail.com>
2008-06-18 14:54:50 +0000

    Minor optimizations and tweaks

--- src/Config.h
+++ src/Config.h
@@ -12,15 +12,15 @@ struct Config
 {
 	bool
 		elitism;				///< Elitism carries the most fit adult over to the next generation
-	double
-		mating_rate,			///< The rate of a successful mating
-		mutation_rate;			///< The rate of mutation
 	unsigned int
 		adult_pool_capacity,	///< Maximum number of adults per generation
 		doomsday,				///< Number of generations
 		lifespan,				///< Number of generations an adult will live
 		max_prodigies,			///< Number of prodigies replacing adults when lifespan > 1
 		offspring_rate;			///< Number of offspring produced each generation
+	double
+		mating_rate,			///< The rate of a successful mating
+		mutation_rate;			///< The rate of mutation
 	std::string
 		plotter;				///< The name of the plotter used
 	std::vector<std::string>
